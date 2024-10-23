b475fb14146a409b7285ad3d9bde10bfe885c876
TekMike365 <miskosklenka@gmail.com>
2024-10-23 11:27:48 +0200

    added mat4

--- src/vmath.py
+++ src/vmath.py
@@ -149,3 +149,137 @@ class Vec4:
 
     def __str__(self) -> str:
         return f"[{self.x}, {self.y}]"
+
+
+class Mat4:
+    def __init__(self) -> None:
+        self.c1 = Vec4(1.0, 0.0, 0.0, 0.0)
+        self.c2 = Vec4(0.0, 1.0, 0.0, 0.0)
+        self.c3 = Vec4(0.0, 0.0, 1.0, 0.0)
+        self.c4 = Vec4(0.0, 0.0, 0.0, 1.0)
+
+    def get_scale(self) -> Vec3:
+        return Vec3(self.c1.x, self.c2.y, self.c3.z)
+
+    def get_position(self) -> Vec3:
+        return Vec3(self.c4.x, self.c4.y, self.c4.z)
+
+    def translate(self, v3: Vec3):
+        self.c4.add(Vec4(v3.x, v3.y, v3.z, 0.0))
+        return self
+
+    def scale(self, v3: Vec3):
+        self.c4.add(Vec4(v3.x, 0.0, 0.0))
+        self.c4.add(Vec4(0.0, v3.y, 0.0, 0.0))
+        self.c4.add(Vec4(0.0, 0.0, v3.z, 0.0))
+        return self
+
+    def rotate_mat4(self, angle: Vec3):
+        sin = Vec3(math.sin(angle.x), math.sin(angle.y), math.sin(angle.z))
+        cos = Vec3(math.cos(angle.x), math.cos(angle.y), math.cos(angle.z))
+
+        rot_x = Mat4()
+        rot_x.c2.y = cos.x
+        rot_x.c2.z = sin.x
+        rot_x.c3.y = -sin.x
+        rot_x.c3.z = cos.x
+
+        rot_y = Mat4()
+        rot_y.c1.x = cos.x
+        rot_y.c3.x = sin.x
+        rot_y.c1.z = -sin.x
+        rot_y.c3.z = cos.x
+
+        rot_z = Mat4()
+        rot_z.c1.y = cos.x
+        rot_z.c1.z = sin.x
+        rot_z.c2.y = -sin.x
+        rot_z.c2.z = cos.x
+
+        self.mul(rot_x).mul(rot_y).mul(rot_z)
+        return self
+
+    def copy(self):
+        new = Mat4()
+        new.c1 = self.c1
+        new.c2 = self.c2
+        new.c3 = self.c3
+        new.c4 = self.c4
+        return new
+
+    def mul(self, other):
+        result = Mat4()
+        result.c1 = Vec4(
+            self.c1.x * other.c1.x
+            + self.c2.x * other.c1.y
+            + self.c3.x * other.c1.z
+            + self.c4.x * other.c1.w,
+            self.c1.y * other.c1.x
+            + self.c2.y * other.c1.y
+            + self.c3.y * other.c1.z
+            + self.c4.y * other.c1.w,
+            self.c1.z * other.c1.x
+            + self.c2.z * other.c1.y
+            + self.c3.z * other.c1.z
+            + self.c4.z * other.c1.w,
+            self.c1.w * other.c1.x
+            + self.c2.w * other.c1.y
+            + self.c3.w * other.c1.z
+            + self.c4.w * other.c1.w,
+        )
+        result.c1 = Vec4(
+            self.c1.x * other.c2.x
+            + self.c2.x * other.c2.y
+            + self.c3.x * other.c2.z
+            + self.c4.x * other.c2.w,
+            self.c1.y * other.c2.x
+            + self.c2.y * other.c2.y
+            + self.c3.y * other.c2.z
+            + self.c4.y * other.c2.w,
+            self.c1.z * other.c2.x
+            + self.c2.z * other.c2.y
+            + self.c3.z * other.c2.z
+            + self.c4.z * other.c2.w,
+            self.c1.w * other.c2.x
+            + self.c2.w * other.c2.y
+            + self.c3.w * other.c2.z
+            + self.c4.w * other.c2.w,
+        )
+        result.c1 = Vec4(
+            self.c1.x * other.c3.x
+            + self.c2.x * other.c3.y
+            + self.c3.x * other.c3.z
+            + self.c4.x * other.c3.w,
+            self.c1.y * other.c3.x
+            + self.c2.y * other.c3.y
+            + self.c3.y * other.c3.z
+            + self.c4.y * other.c3.w,
+            self.c1.z * other.c3.x
+            + self.c2.z * other.c3.y
+            + self.c3.z * other.c3.z
+            + self.c4.z * other.c3.w,
+            self.c1.w * other.c3.x
+            + self.c2.w * other.c3.y
+            + self.c3.w * other.c3.z
+            + self.c4.w * other.c3.w,
+        )
+        result.c1 = Vec4(
+            self.c1.x * other.c4.x
+            + self.c2.x * other.c4.y
+            + self.c3.x * other.c4.z
+            + self.c4.x * other.c4.w,
+            self.c1.y * other.c4.x
+            + self.c2.y * other.c4.y
+            + self.c3.y * other.c4.z
+            + self.c4.y * other.c4.w,
+            self.c1.z * other.c4.x
+            + self.c2.z * other.c4.y
+            + self.c3.z * other.c4.z
+            + self.c4.z * other.c4.w,
+            self.c1.w * other.c4.x
+            + self.c2.w * other.c4.y
+            + self.c3.w * other.c4.z
+            + self.c4.w * other.c4.w,
+        )
+        self = result
+        return self
