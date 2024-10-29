7a853293d752c48ba2e73740f868024fdb9eb0dd
TekMike365 <miskosklenka@gmail.com>
2024-10-29 07:59:21 +0100

    inverse cube

--- cube.py
+++ cube.py
@@ -3,14 +3,61 @@ import renderer as re
 
 # Define vertices of the cube
 vertices = [
-    [re.Vec3(-0.5, -0.5, 0.0), re.Vec3(0.0, 0.0, 0.0)],
-    [re.Vec3(0.5, -0.5, 0.0), re.Vec3(1.0, 0.0, 0.0)],
-    [re.Vec3(0.5, 0.5, 0.0), re.Vec3(0.0, 1.0, 0.0)],
-    [re.Vec3(-0.5, 0.5, 0.0), re.Vec3(0.0, 0.0, 1.0)],
+    [re.Vec3(-0.5, -0.5, -0.5), re.Vec3(0.0, 1.0, 0.0)],
+    [re.Vec3(0.5, -0.5, -0.5), re.Vec3(0.0, 0.0, 1.0)],
+    [re.Vec3(0.5, -0.5, 0.5), re.Vec3(1.0, 1.0, 1.0)],
+    [re.Vec3(-0.5, -0.5, 0.5), re.Vec3(1.0, 1.0, 1.0)],
+    [re.Vec3(-0.5, 0.5, -0.5), re.Vec3(1.0, 0.0, 0.0)],
+    [re.Vec3(0.5, 0.5, -0.5), re.Vec3(1.0, 1.0, 1.0)],
+    [re.Vec3(0.5, 0.5, 0.5), re.Vec3(1.0, 1.0, 1.0)],
+    [re.Vec3(-0.5, 0.5, 0.5), re.Vec3(1.0, 1.0, 1.0)],
 ]
 
 # Define indices for triangles forming the cube
-indices = [0, 1, 2, 2, 3, 0]
+indices = [
+    # front
+    0,
+    1,
+    5,
+    5,
+    4,
+    0,
+    # right
+    1,
+    2,
+    6,
+    6,
+    5,
+    1,
+    # back
+    2,
+    3,
+    7,
+    7,
+    6,
+    2,
+    # left
+    3,
+    0,
+    4,
+    4,
+    7,
+    3,
+    # top
+    6,
+    7,
+    4,
+    4,
+    5,
+    6,
+    # bottom
+    1,
+    0,
+    3,
+    3,
+    2,
+    1,
+]
 
 re.set_screen_size(re.Vec2(640, 420))
 
@@ -26,7 +73,7 @@ def vertex_shader(atribs: list[Any], uniforms: dict) -> list[Any]:
     camera: re.Camera = uniforms["camera"]
 
     pos = a_pos
-    # pos = camera.world2screen_pos(a_pos)
+    pos = camera.world2screen_pos(a_pos)
 
     return [pos, a_color]
 
